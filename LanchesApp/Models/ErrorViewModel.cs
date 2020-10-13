818c074419bcabd2ebc07134593f7290818f4a76
Miter Mayer Ferreira <mmof@hifa.corp>
2020-10-13 18:35:09 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LanchesApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
